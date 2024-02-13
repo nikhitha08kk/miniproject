c70d4f3b917d2d27af4eb641330af01fdd2928d1
nikhitha08kk <nikhithakakkadan07@gmail.com>
2024-02-13 14:22:08 +0700

    headerstarting

---
+++ src/Components/User/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div class="Header">
+        <div class="head">Name</div>
+        <img class="logo"></img>
+        <div class="nav">
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Router/Userrouter.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import UserHome from '../Pages/User/UserHome'
+import UserSignUp from "../Pages/User/UserSignUp"
+import UserLogin from '../Pages/User/UserLogin'
+import UserProduct from '../Pages/User/UserProduct'
+
+
+
+function Userrouter() {
+  return (
+    <div>
+        <Routes>
+        <Route path='/' element={<UserHome/>}/>
+         <Route path='/signup' element={<UserSignUp/>}/>
+        <Route path='/login' element={<UserLogin/>}/>
+        <Route path='/product' element={<UserProduct/>}/>
+        
+         
+        </Routes>
+    </div>
+  )
+}
+
+export default Userrouter
