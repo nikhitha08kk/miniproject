692f4e7751913a3b942851f1c72c62047d293680
nikhitha08kk <nikhithakakkadan07@gmail.com>
2024-02-20 12:50:30 +0700

    home page

--- src/Components/User/Header/Header.jsx
+++ src/Components/User/Header/Header.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaUser, FaSearch, FaCartPlus } from 'react-icons/fa';
+import { IoHeartOutline } from 'react-icons/io5'; 
 
 function Header() {
+  const iconSize = 24; // Set the size of the icons (in pixels)
+
+
   return (
-    <div class="Header">
-        <div class="head">Name</div>
-        <img class="logo"></img>
-        <div class="nav">
-
+    <div className="Header">
+      <div className="container">
+        <div className='row'>
+          <div className="col-9 head">Euphoria</div>
+          <div className="col-3">
+            <span className='iconbar'><FaSearch size={iconSize} /></span>
+            <span className='iconbar'><FaUser size={iconSize} /></span>
+            <span className='iconbar'><IoHeartOutline size={iconSize} /></span>
+            <span className='iconbar'><FaCartPlus size={iconSize} /></span>
+          </div>
         </div>
-
-
+      </div>
+      <div className='container navbar'>
+          <div className='row'>
+            <div className='col'>
+              <div className='nav-item'>NEW ARRIVALS</div>
+            </div>
+            <div className='col'>
+              <div className='nav-item'>BEST SELLER</div>
+            </div>
+            <div className='col'>
+              <div className='nav-item'>MEN</div>
+            </div>
+            <div className='col'>
+              <div className='nav-item'>WOMEN</div>
+            </div>
+            <div className='col'>
+              <div className='nav-item'>BODY & BATH</div>
+            </div>
+            <div className='col'>
+              <div className='nav-item'>MINIATURES</div>
+            </div>
+            <div className='col'>
+              <div className='nav-item'>SHOP BY BRANDS</div>
+            </div>
+          </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/Pages/User/UserHome.jsx
+++ src/Pages/User/UserHome.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
  import Header from '../../Components/User/Header/Header'
+ import Home from '../../Components/User/HomePage/Home'
 function UserHome() {
   return (
-    <div><Header/></div>
+    <div><Header/><Home/></div>
+   
   )
 }
 
