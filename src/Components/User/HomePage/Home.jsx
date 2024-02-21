e9afc9c0231d58e47643a733222e26f7edcbac54
nikhitha08kk <nikhithakakkadan07@gmail.com>
2024-02-22 06:01:31 +0700

    css of home page

--- src/Components/User/HomePage/Home.jsx
+++ src/Components/User/HomePage/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className='home'>
     <div className='container home-img'>
-<img className='bot' src='https://media.istockphoto.com/id/1158358904/photo/spraying-perfume-on-dark-background-closeup-image.jpg?s=612x612&w=0&k=20&c=FgO1tJIxW_fVH0e7YHb-oMb_iDshELnMR6qXGILQFcU='></img>
+<img className='bot' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVX0wd9qUfsP6lHcPQGkT6L5GB6xs79L5KEZZeiQqY_50Icy9s123lwpeXI7Jdve8aWrQ&usqp=CAU'></img>
 
      </div>  
     </div>
